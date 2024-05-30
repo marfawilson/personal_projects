db3ab4409265a28ef4c5d3a6b9263c3abe7affae
Marissa Wilson <marfawilsoncode@gmail.com>
2024-05-30 12:23:07 -0500

    Copied code from standalone repo to personal_projects repo

---
+++ sales_tax_calculator/script.js
@@ -0,0 +1,5 @@
+// Input variables
+let form = document.getElementById('form');
+let beforetax = document.getElementById('before-tax');
+let salestax = document.getElementById('tax-rate');
+let aftertax = document.getElementById('after-tax');
