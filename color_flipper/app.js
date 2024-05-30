49be6c826f3e0d3b4b5ebea2ae877756b41cc592
Marissa Wilson <marfawilsoncode@gmail.com>
2024-05-30 15:39:26 -0500

    Added logic in app.js aka simple page for random background colors on button click

--- color_flipper/app.js
+++ color_flipper/app.js
@@ -1,1 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "#ff00ff", "aquamarine", "rgba(95,158,160)", "gold"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+    // GOAL: get random number between 0-3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    let randomColor = Math.random() * colors.length;
+    return Math.floor(randomColor);
+}
