57cfaffe523ff4235dfafda1777fbdd854171103
Marissa Wilson <marfawilsoncode@gmail.com>
2024-06-04 13:06:45 -0500

    Created NavBar component & imported into App.jsx

--- color_flipper/react/src/App.jsx
+++ color_flipper/react/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import NavBar from './components/NavBar.jsx';
+import './App.css';
 
 function App() {
 
   return (
     <>
-      
+      <NavBar />
     </>
   )
 }
---
+++ color_flipper/react/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <div className="nav-center">
+                <h4>Color Flipper</h4>
+                <ul className="navlinks">
+                    <li><a href="">Simple</a></li>
+                    <li><a href="">Hex</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
