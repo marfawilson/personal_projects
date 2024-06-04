626080677504dbe97d65066eb3795c50b2f43957
Marissa Wilson <marfawilsoncode@gmail.com>
2024-06-04 13:21:52 -0500

    Created Main component & imported into App.jsx

--- color_flipper/react/src/App.jsx
+++ color_flipper/react/src/App.jsx
@@ -1,4 +1,5 @@
 import NavBar from './components/NavBar.jsx';
+import Main from './components/Main.jsx';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <Main />
     </>
   )
 }
---
+++ color_flipper/react/src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <div className="container">
+                <h2>Background Color : <span className="color">#f1f5f8</span></h2>
+                <button className="btn btn-hero" id = "btn">click me!</button>
+            </div>
+        </main>
+    )
+}
